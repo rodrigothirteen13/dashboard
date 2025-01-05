f6d2334c05a1cc94796d3d379c75db605455867f
Rodrigo_Gonçalves <rodrigoncalves.net@outlook.com>
2025-01-05 18:25:34 -0300

    parte de grafico finalizada

--- src/Components/GraficoVendas.tsx
+++ src/Components/GraficoVendas.tsx
@@ -1,32 +1,36 @@
 import { Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts'
 import { IVenda } from '../Context/DataContext'
 
-const dadosGraficos = [
-  {
-    data: "2023-05-03",
-    pago: 3000,
-    processando: 300,
-    falha: 20,
-  },
-  {
-    data: "2023-05-04",
-    pago: 3400,
-    processando: 500,
-    falha: 60,
-  },
-  {
-    data: "2023-05-05",
-    pago: 2000,
-    processando: 500,
-    falha: 60,
-  }
-]
+type VendaDia = {
+  data: string,
+  pago: number,
+  processando: number,
+  falha: number
+}
+
+function transformData(data: IVenda[]): VendaDia[] {
+  const dias = data.reduce((acc: { [key: string]: VendaDia }, item) => {
+    const dia = item.data.split(" ")[0]
+    if(!acc[dia]) {
+      acc[dia] = {
+        data: dia,
+        pago: 0,
+        falha: 0,
+        processando: 0,
+      };
+    }
+    acc[dia][item.status] += item.preco
+    return acc
+  }, {});
+  
+  return Object.values(dias).map((dia) => ({...dia, data: dia.data.substring(5)}))
+}
 
 const GraficoVendas = ({data}: {data: IVenda[]}) => {
-  
+  const transformedDta = transformData(data)
   return (
     <ResponsiveContainer width={'99%'} height={400}>
-      <LineChart width={400} height={400} data={dadosGraficos}>
+      <LineChart width={400} height={400} data={transformedDta}>
         <XAxis dataKey="data" />
         <YAxis />
         <Tooltip />
